2eb52be58bd03cc7e00d8d776401138246a4a959
wen <sunzibinfa@gmail.com>
2016-06-27 22:03:22 +0800

    models關聯

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
 	searchkick
 	has_many :reviews, dependent: :destroy
+	belongs_to :user
 
 	has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
