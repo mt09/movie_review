5d6d97be723faa02f91c6497a918c1672aeb070c
wen <sunzibinfa@gmail.com>
2016-06-27 15:47:16 +0800

    新增movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+
+		if @movie.save
+			redirect_to movies_path, info: "nice movie"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:title, :description, :movie_length, :director, :rating,
+																	:image)
+	end
 end
