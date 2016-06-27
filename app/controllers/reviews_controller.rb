7f64da7ccb72f086c109a2ac79e882a1b6965f11
wen <sunzibinfa@gmail.com>
2016-06-27 16:28:05 +0800

    add new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+	def new
+		@movie = Movie.find(params[:movie_id])
+		@review = @movie.reviews.new
+	end
+
+	def create
+		@movie = Movie.find(params[:movie_id])
+		@review = @movie.reviews.build(review_params)
+
+		if @review.save
+			redirect_to movie_path(@movie, @review), info: "Successfully posted new review."
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def review_params
+		params.require(:review).permit(:rating, :comment)
+	end
 end
