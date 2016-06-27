2f98a94cc101bde2b7438604bb6ecdc27f52dfdc
wen <sunzibinfa@gmail.com>
2016-06-27 17:19:10 +0800

    gem paperclip, generate migration of paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627080725) do
+ActiveRecord::Schema.define(version: 20160627091837) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160627080725) do
     t.string   "length"
     t.string   "director"
     t.string   "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "reviews", force: :cascade do |t|
