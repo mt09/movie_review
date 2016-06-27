68a9785033d3cfeae30fef44cb953d77ffc36741
wen <sunzibinfa@gmail.com>
2016-06-27 16:09:41 +0800

    重建model review

--- db/migrate/20160627080725_create_reviews.rb
+++ db/migrate/20160627080725_create_reviews.rb
@@ -1,8 +1,9 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
+      t.integer :movie_id
+      t.text :comment
       t.integer :rating
-      t.string :comment
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627075749) do
+ActiveRecord::Schema.define(version: 20160627080725) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20160627075749) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer  "movie_id"
+    t.text     "comment"
     t.integer  "rating"
-    t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
