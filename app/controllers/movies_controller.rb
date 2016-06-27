32bccb0bfa4edcecebc9b74c0547c8fb3b9e87e5
wen <sunzibinfa@gmail.com>
2016-06-27 15:50:29 +0800

    編輯movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,23 @@ class MoviesController < ApplicationController
 		end
 	end
 
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+
+		if @movie.update(movie_params)
+			redirect_to movies_path
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def movie_params
-		params.require(:movie).permit(:title, :description, :movie_length, :director, :rating,
-																	:image)
+		params.require(:movie).permit(:title, :description, :length, :director, :rating)
 	end
 end
